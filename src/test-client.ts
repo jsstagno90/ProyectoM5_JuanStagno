2458766ebd80e50c22dba2520044fdd80ad2a66b
Juan Stagno <jsstagno@hotmail.com>
2026-08-10 20:53:49 -0300

    feat: implement create repository tool

---
+++ src/test-client.ts
@@ -0,0 +1,26 @@
+import { Client } from "@modelcontextprotocol/sdk/client/index.js";
+import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js";
+
+const client = new Client({
+  name: "github-ai-agent-test-client",
+  version: "1.0.0",
+});
+
+const transport = new StdioClientTransport({
+  command: "tsx",
+  args: ["src/server.ts"],
+});
+
+await client.connect(transport);
+
+const result = await client.callTool({
+  name: "create_repository",
+  arguments: {
+    name: "mcp-test-repository",
+    description: "Repositorio creado mediante nuestro MCP Server",
+  },
+});
+
+console.error(result);
+
+await transport.close();
